d11d0b1756110cf4fad08b8fbcbd79d1b660c0e0
shashank singh <shashanksingh.co243@gmail.com>
2025-09-23 23:01:08 +0530

    Basic hello world

---
+++ basics/hello-world.go
@@ -0,0 +1,6 @@
+package main;
+import "fmt"; //Package fmt implements formatted I/O with functions analogous to C's printf and scanf. The format 'verbs' are derived from C's but are simpler.
+
+func main() {
+	fmt.Println("hello world");
+}
