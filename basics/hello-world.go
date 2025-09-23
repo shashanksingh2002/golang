e813cbcaeb9ca058ef73f70288504aa6a6a3db57
shashank singh <shashanksingh.co243@gmail.com>
2025-09-23 23:12:04 +0530

    add-comments

--- basics/hello-world.go
+++ basics/hello-world.go
@@ -1,6 +1,7 @@
-package main;
+package main; //Every Go program is made up of packages. Programs start running in package main.
 import "fmt"; //Package fmt implements formatted I/O with functions analogous to C's printf and scanf. The format 'verbs' are derived from C's but are simpler.
 
-func main() {
+//func main is the function that gets called when the program starts. Every Go program must have a main function in the main package.
+func main() { 
 	fmt.Println("hello world");
 }
