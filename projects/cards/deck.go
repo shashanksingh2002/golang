c018cb308a759d7a1b9fd67b5cc46a83dce79866
shashank singh <shashanksingh.co243@gmail.com>
2025-09-25 01:40:58 +0530

    init cards

--- projects/cards/deck.go
+++ projects/cards/deck.go
@@ -1,35 +1,62 @@
-package main;
+package main
 
-type deck []string;
+import (
+	"os"
+)
+
+type deck []string
 
 func NewDeck() deck {
 	cardDeck := deck{}
 
-	cardNames := []string {"Heart", "Diamond", "Club", "Spade"};
-	cardValues := []string {"Ace", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "King", "Queen", "Joker"}
+	cardNames := []string{"Heart", "Diamond", "Club", "Spade"}
+	cardValues := []string{"Ace", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "King", "Queen", "Joker"}
 
-	for _,cName := range cardNames {
-		for _,cValue := range cardValues {
-			cardDeck = append(cardDeck, cValue + " of " + cName);
+	for _, cName := range cardNames {
+		for _, cValue := range cardValues {
+			cardDeck = append(cardDeck, cValue+" of "+cName)
 		}
 	}
 
-	return cardDeck;
+	return cardDeck
 }
 
-func (d deck) Print () {
+func (d deck) Print() {
 	for _, card := range d {
 		println(card)
 	}
 }
 
 func (d deck) Shuffle() {
-	
+
 }
 
 func (d deck) Deal(handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
+func (d deck) Save(fileName string, data []byte, perm os.FileMode) error {
+	return os.WriteFile(fileName, data, perm)
+}
 
+func (d deck) ToString() string {
+	result := "";
 
+	for _, card:= range d {
+		result += card + ","
+	}
+
+	return result;
+}
+
+func (d deck) Convert2Byte() []byte {
+	result := []byte{};
+	for _, card := range d {
+		result = append(result, []byte(card)...)
+	}
+	return result;
+}
+
+func ReadFile(fileName string) ([]byte, error) {
+	return os.ReadFile(fileName)
+}
--- projects/cards/main.go
+++ projects/cards/main.go
@@ -1,12 +1,24 @@
-package main;
+package main
+
+import ("fmt")
 
 func main() {
-	cardDeck := NewDeck();
+	randomString := "Hello, World!"
+	fmt.Println([]byte(randomString))
+	cardDeck := NewDeck()
 	cardDeck.Print()
-	deal1, deal2 := cardDeck.Deal(5);
+	deal1, deal2 := cardDeck.Deal(5)
 
 	println("------- Deal 1 -------")
-	deal1.Print();
+	deal1.Print()
 	println("------- Deal 2 -------")
-	deal2.Print();
+	deal2.Print()
+	cardDeck.Save("my_cards.txt", cardDeck.Convert2Byte(), 0666)
+	byteSlice, err := ReadFile("my_cards.txt");
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("File contents:", string(byteSlice))
+	}
 }
